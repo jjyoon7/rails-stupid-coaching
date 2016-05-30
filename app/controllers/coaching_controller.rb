a4ff9b4dbce654e71a8858e91d041ae38d9ee289
Jay Yoon <jay.yoon7@gmail.com>
2016-05-30 18:21:51 +0200

    fixed coacing

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -3,5 +3,33 @@ class CoachingController < ApplicationController
   end
 
   def answer
+    @ask = params[:query]
+    @answer = coach_answer_enhanced(@ask)
   end
+
+  def coach_answer(your_message)
+    if your_message.downcase == "i am going to work right now!"
+      return ""
+    elsif your_message.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def coach_answer_enhanced(your_message)
+    if your_message == "I AM GOING TO WORK RIGHT NOW!"
+      return ""
+    elsif your_message == your_message.upcase
+      return "I can feel your motivation! #{coach_answer(your_message)}"
+    else
+      coach_answer(your_message)
+  #   your_message.upcase.end_with?("?")
+  #   return "I can feel your motivation! I don't care, get dressed and go to work!"
+  # # elsif your_message.coach_answer.upcase
+  # #   return "I can feel your motivation! Silly question, get dressed and go to work!"
+  # else
+end
+end
+
 end
